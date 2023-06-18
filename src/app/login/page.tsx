fe2f5d4c1aa4ae97916f3b0701e5c4ee2052a640
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-06-18 08:51:53 +0200

    Don't causes a problem replace the ' with &apos;

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,7 +20,7 @@ const page = () => {
           <div className="flex w-full items-center justify-between sm:w-72 md:justify-evenly lg:w-80">
             <div className="h-0 w-10 border-[1px] border-gray-300 sm:w-10 lg:w-14"></div>
             <div className="sm:w-54  md:text-md text-sm">
-              Don't have a Halali account?
+              Don&apos;t have a Halali account?
             </div>
             <div className="flex h-0 w-10 border-[1px] border-gray-300 sm:w-10 lg:w-14"></div>
           </div>
