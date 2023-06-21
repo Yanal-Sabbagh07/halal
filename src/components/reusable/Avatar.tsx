6cc92d883550a341ab35274163e50369c3292bb0
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-06-21 20:59:31 +0200

    fix avatar arrow position on small screens with changing colors and hoovers

--- src/components/reusable/Avatar.tsx
+++ src/components/reusable/Avatar.tsx
@@ -21,29 +21,29 @@ const Avatar = ({ userName }: IAvatarprops) => {
 
       <div
         className={`${profileClicked && "scale-100 opacity-100"} 
-        absolute right-0 top-[60px] h-64  w-56 scale-0 rounded-xl border-2 border-gray-100  
-         bg-slate-50 text-white  opacity-0 transition-all after:absolute 
-        after:-top-8 after:right-4 after:border-[16px] after:border-b-gray-100 after:border-l-transparent after:border-r-transparent
+        absolute right-0 top-[60px] h-56  w-56 scale-0 rounded-xl border-2 border-slate-200  
+         bg-slate-200 text-white  opacity-0 transition-all after:absolute 
+        after:-top-8 after:right-2 after:border-[16px] after:border-b-slate-200 after:border-l-transparent after:border-r-transparent
         after:border-t-transparent after:transition-all lg:-right-4 lg:after:right-[26px]`}
       >
         <div className="flex h-full w-full flex-col items-center justify-center">
           <div className="flex h-[95%] w-[95%] flex-col items-center justify-between rounded-xl bg-white">
-            <div className="flex h-32 w-full flex-col items-center justify-center  border-b-2 border-slate-200">
+            <div className="flex h-28 w-full flex-col items-center justify-evenly  border-b-2 border-slate-200">
               <AccountCircleIcon
-                style={{ width: "86px", height: "86px" }}
+                style={{ width: "76px", height: "76px" }}
                 className="  text-blue-800"
               />
               <p className="text-gray-700"> {userName} </p>
             </div>
             <Link
               href="#"
-              className="flex h-12 w-full cursor-pointer items-center justify-center rounded-lg bg-white   text-slate-900 hover:bg-slate-200 hover:font-extrabold md:flex  lg:w-full lg:rounded-sm "
+              className="flex h-12 w-full cursor-pointer items-center justify-center rounded-none bg-white   text-slate-900 hover:bg-slate-100 hover:font-semibold md:flex  lg:w-full  "
             >
               Porfile
             </Link>
             <button
               onClick={() => signOut()}
-              className=" flex h-12 w-full cursor-pointer items-center justify-center rounded-lg bg-white  text-slate-900 hover:bg-slate-200 hover:font-extrabold md:flex  lg:w-full lg:rounded-sm "
+              className=" flex h-12 w-full cursor-pointer items-center justify-center  rounded-b-xl	  bg-white text-slate-900 hover:bg-slate-100 hover:font-semibold  md:flex lg:w-full "
             >
               Sign out
             </button>
