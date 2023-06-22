23d56f451865304ac379ade4c4d8702e1a7fe7c8
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-06-22 11:49:28 +0200

    add a new logo for tablet and larg screens

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 const Logo = () => {
   return (
-    <div className="flex w-16   h-14   items-center justify-center lg:justify-start">
-      <Image
-        src="/logo2.png"
-        width={50}
-        height={50}
-        alt="logo"
-        placeholder="blur"
-        blurDataURL={"/logo2.png"}
-      />
-    </div>
+    <>
+      <div className="hidden h-full w-64 items-center justify-center  sm:flex lg:justify-start">
+        <Link href="/">
+          <Image
+            src="/logo-l.png"
+            width={250}
+            height={56}
+            alt="logo"
+            placeholder="blur"
+            blurDataURL={"/logo-l.png"}
+            className=" mt-1"
+          />
+        </Link>
+      </div>
+      <div className="flex h-14 w-14 items-center justify-center sm:hidden ">
+        <Link href="/">
+          <Image
+            src="/logo2.png"
+            width={50}
+            height={50}
+            alt="logo"
+            placeholder="blur"
+            blurDataURL={"/logo2.png"}
+          />
+        </Link>
+      </div>
+    </>
   );
 };
 
