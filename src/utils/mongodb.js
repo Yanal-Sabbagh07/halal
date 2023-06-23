ca926fa3e7371a55c4562fd970f1fda85d854c9b
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-06-23 03:21:15 +0200

    edit mongodb.js to fix google provider on deployment

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -11,8 +11,18 @@ const options = {};
 let client;
 let clientPromise;
 
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
